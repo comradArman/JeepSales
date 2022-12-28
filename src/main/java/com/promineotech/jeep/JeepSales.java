7fe0e2195549e6c52587917269af68cb593ca649
unknown <armanhammer12@gmail.com>
2022-12-28 14:44:33 -0700

    week 14 error

--- src/main/java/com/promineotech/jeep/JeepSales.java
+++ src/main/java/com/promineotech/jeep/JeepSales.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JeepSales {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		
 		SpringApplication.run(JeepSales.class, args);
 		
--- src/main/java/com/promineotech/jeep/controller/BasicJeepSalesController.java
+++ src/main/java/com/promineotech/jeep/controller/BasicJeepSalesController.java
@@ -2,16 +2,30 @@ package com.promineotech.jeep.controller;
 
 import java.util.List;
 
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
 
+
+import lombok.extern.slf4j.Slf4j;
+
+
+
+
 @RestController
+@Slf4j 
 public class BasicJeepSalesController implements JeepSalesController {
 
 	@Override
 	public List<Jeep> fetchJeeps(JeepModel model, String trim) {
+		
+		//Log.info("model={}, trim={}", model, trim);
+		log.info();
+		
+		
+	
 		
 		return null;
 	}
